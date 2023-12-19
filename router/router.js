63dce8849da6abf1816cbb0531bbc5e4e87e8f21
Vinay nayak <71583413+vinaynayak3344@users.noreply.github.com>
2023-12-19 16:16:14 +0530

    Update router.js

--- router/router.js
+++ router/router.js
@@ -6,28 +6,45 @@ const router = express.Router()
 const fs = require('fs').promises;
 const path = require('path');
 
-const directoryPath = path.join(__dirname, 'Temp');
+const directoryPath = '/opt/render/project/src/router/Temp';
 
-// Read the contents of the directory
-fs.readdir(directoryPath)
-    .then(files => {
+async function deleteFiles() {
+    try {
+        // Check if the directory exists
+        await fs.access(directoryPath);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            console.error(`Error: Directory ${directoryPath} does not exist`);
+            // Optionally, you can create the directory here using fs.mkdir
+            // await fs.mkdir(directoryPath);
+            process.exit(1); // Exit the script with an error code
+        } else {
+            console.error('Error:', error);
+            process.exit(1); // Exit the script with an error code
+        }
+    }
+
+    try {
+        // Read the contents of the directory
+        const files = await fs.readdir(directoryPath);
+
         // Delete each file
-        const unlinkPromises = files.map(file => {
+        const unlinkPromises = files.map(async (file) => {
             const filePath = path.join(directoryPath, file);
-            return fs.unlink(filePath)
-                .then(() => console.log(`File ${filePath} deleted successfully`))
-                .catch(err => console.error(`Error deleting file ${filePath}:`, err));
+            await fs.unlink(filePath);
+            console.log(`File ${filePath} deleted successfully`);
         });
 
         // Wait for all unlink promises to resolve
-        return Promise.all(unlinkPromises);
-    })
-    .then(() => {
+        await Promise.all(unlinkPromises);
+
         console.log('All files deleted successfully');
-    })
-    .catch(err => {
+    } catch (err) {
         console.error('Error:', err);
-    });
+    }
+}
+
+deleteFiles();
 
 
 
